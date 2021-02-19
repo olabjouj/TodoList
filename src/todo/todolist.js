f31cc3a889fb3ac848d865dcc4cf4948eb349636
Oumayma LABJOUJ <oumayma.labjouj@gmail.com>
2021-02-19 19:07:09 +0100

    my last commit todoList

--- src/todo/todolist.js
+++ src/todo/todolist.js
@@ -2,38 +2,38 @@ import React, { Component } from 'react';
 
 
 class TodoList extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            userInput : "",
-            items:[]
+            userInput: "",
+            items: []
         };
     }
-    onChange(event){
+    onChange(event) {
         this.setState({
             userInput: event.target.value
-        }, ()=> console.log(this.state.userInput));
+        }, () => console.log(this.state.userInput));
     }
-    addTodo(event){
+    addTodo(event) {
         event.preventDefault();
         this.setState({
-            userInput :'',
-            items : [...this.state.items, this.state.userInput]
+            userInput: '',
+            items: [...this.state.items, this.state.userInput]
         });
     }
-    deletTodo(event){
+    deletTodo(event) {
         event.preventDefault();
-        const array= this.state.items;
+        const array = this.state.items;
         const index = array.indexOf(event.target.value);
         array.splice(index, 1);
         this.setState({
             items: array
         });
     }
-    renderTodos( ){ 
-        return this.state.items.map((item)=>{
-            return(
-                <div key={item}>
+    renderTodos() {
+        return this.state.items.map((item) => {
+            return (
+                <div classeName='list-group-item'key={item}>
                     {item} | <button onClick={this.deletTodo.bind(this)}>X</button>
                 </div>
             );
@@ -42,21 +42,25 @@ class TodoList extends Component {
     render() {
         return (
             <div>
-                <h1>Ma list Todo</h1>
-                <form>
-                    <input type="text" 
-                            placeholder="Renseigner ton Item" 
-                            value={this.state.userInput}
-                            onChange={this.onChange.bind(this)}/>
-                    <button onClick={this.addTodo.bind(this)}>Ajouter</button>
-                    
+                <h1 align='center'>Ma list Todo</h1>
+                <form className='form-row align-items-center'>
+                    <input type="text"
+                        placeholder="Renseigner ton Item"
+                        value={this.state.userInput}
+                        onChange={this.onChange.bind(this)}
+                        className='form-control mb-2'
+                    />
+                    <button 
+                    classeName='btn btn-danger '
+                    onClick={this.addTodo.bind(this)}>Ajouter</button>
+
                 </form>
-                <div>
+                <div className='list-group'>
                     {this.renderTodos()}
                 </div>
             </div>
 
         );
-    } 
+    }
 }
 export default TodoList;
